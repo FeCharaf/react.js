d6b30270c81b980580699d516ae14b2e5333142e
FeCharaf <felipecharaf@gmail.com>
2024-03-14 17:52:27 -0300

    first steps

---
+++ meu-projeto/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Olá</h1>
+        <p>Meu aplicativo React</p>
+    </div>
+  );
+}
+
+export default App;
