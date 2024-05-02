3a8349dfeda7276634891ab617acfaa72c30bf3f
FeCharaf <felipecharaf@gmail.com>
2024-05-02 18:21:54 -0300

    aula07 fragments

---
+++ meu-projeto/src/components/List.js
@@ -0,0 +1,15 @@
+function List(props) {
+    return (
+    <>
+     <h1>Minha Lista</h1>
+     <ul>
+        <li>Item1</li>
+        <li>Item2</li>
+        <li>Item3</li>
+     </ul>
+    
+    </>
+    )
+}
+
+export default List
