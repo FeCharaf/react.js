64554ac22952371efe48f4865f3c204a6a214cf4
FeCharaf <felipecharaf@gmail.com>
2024-05-02 18:03:41 -0300

    aula06

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import SayMyName from "./components/SayMyName";
 import Pessoa from "./components/Pessoa";
+import Frase from "./components/Frase";
 
 function App() {
   const nome = "Maria";
   return (
     <div className="App">
+      <Frase />
       <SayMyName nome="Felipe" />
       <SayMyName nome="João" />
       <SayMyName nome={nome} />
