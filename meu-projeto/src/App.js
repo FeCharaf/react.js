9658b01802f8b3a4630c0a8742782b4e7ac81dfa
FeCharaf <felipecharaf@gmail.com>
2024-05-13 11:41:13 -0300

    testando eventos

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -4,13 +4,14 @@ import Pessoa from "./components/Pessoa";
 import Frase from "./components/Frase";
 import List from "./components/List";
 import Evento from "./components/Evento";
+import Form from "./components/Form";
 
 function App() {
   const nome = "Maria";
   return (
     <div className="App">
-      <h1>Testando eventos</h1>
-      <Evento />
+      <h1>Testando formulário</h1>
+      <Form />
     </div>
   );
 }
