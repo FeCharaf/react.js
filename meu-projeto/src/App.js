458083ce0ae9e0fa43431a0b7aaa3e8d5b3af025
FeCharaf <felipecharaf@gmail.com>
2024-07-26 19:09:36 -0300

    Condicional

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
-import SayMyName from "./components/SayMyName";
-import Pessoa from "./components/Pessoa";
-import Frase from "./components/Frase";
-import List from "./components/List";
-import Evento from "./components/Evento";
+// import SayMyName from "./components/SayMyName";
+// import Pessoa from "./components/Pessoa";
+// import Frase from "./components/Frase";
+// import List from "./components/List";
+// import Evento from "./components/Evento";
 import Form from "./components/Form";
 
 function App() {
---
+++ meu-projeto/src/components/Condicional.js
@@ -0,0 +1,8 @@
+import {useState} from 'react'
+
+function Condicional() {
+
+
+}
+
+export default Condicional
